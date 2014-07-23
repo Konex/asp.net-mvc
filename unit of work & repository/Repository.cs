2b18de01f2e0f5322268275099275cf5647aa482
Yini <yinnski@gmail.com>
2014-07-23 15:20:17 +0200

    add generic repository and unit of work class

---
+++ unit of work & repository/Repository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using vDieu.Dal.Contracts;
+
+namespace vDieu.Dal
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        internal IDbContext Context;
+        internal IDbSet<T> DbSet;
+
+        public Repository(IDbContext context)
+        {
+            Context = context;
+            DbSet = context.Set<T>();
+        }
+
+        public virtual IQueryable<T> GetDbQueryable()
+        {
+            return DbSet.AsQueryable();
+        }
+
+        public virtual IQueryable<T> GetDbQueryable(string includeProperties = "")
+        {
+            if (includeProperties == null) throw new ArgumentNullException("includeProperties");
+
+            return includeProperties.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries)
+                .Aggregate<string, IQueryable<T>>(DbSet, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+        public virtual IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<T> query = DbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            return orderBy != null ? orderBy(query).ToList() : query.ToList();
+        }
+
+        public virtual T GetByID(object id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual T Insert(T entity)
+        {
+            return DbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            T entityToDelete = DbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(T entityToDelete)
+        {
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+            DbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(T oldEntity, T newEntity)
+        {
+            Context.Entry(oldEntity).CurrentValues.SetValues(newEntity);
+        }
+    }
+}
---
+++ unit of work & repository/UnitOfWork.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using vDieu.Dal.Contracts;
+
+namespace vDieu.Dal
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbContext _context;
+        private Hashtable _repositories;
+        private bool _disposed;
+
+        public UnitOfWork(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public UnitOfWork()
+        {
+            _context = new vDbContext();
+        }
+
+        public IRepository<T> Repository<T>() where T : class
+        {
+            if (_repositories == null)
+                _repositories = new Hashtable();
+
+            var type = typeof(T).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(Repository<>);
+
+                var repositoryInstance =
+                    Activator.CreateInstance(repositoryType
+                            .MakeGenericType(typeof(T)), _context);
+
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IRepository<T>)_repositories[type];
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
