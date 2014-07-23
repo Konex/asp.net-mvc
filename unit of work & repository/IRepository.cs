032b3649fb661c8fc7c5799da8b512c66ddc335d
Yini <yinnski@gmail.com>
2014-07-23 16:03:40 +0200

    add repository and unit of work interfaces

---
+++ unit of work & repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace vDieu.Dal.Contracts
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetDbQueryable();
+        IQueryable<T> GetDbQueryable(string includeProperties = "");
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, 
+                              Func<IQueryable<T>, IOrderedQueryable<T>> orderby = null,
+                              string includeProperties = "");
+        T GetByID(object id);
+        T Insert(T e);
+        void Delete(object id);
+        void Delete(T e);
+        void Update(T oE, T nE);
+    }
+}
---
+++ unit of work & repository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace vDieu.Dal.Contracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> Repository<T>() where T : class;
+        void Save();
+        void Dispose();
+    }
+}
