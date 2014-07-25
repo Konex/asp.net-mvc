7b010a1103b84b4c64561dcd2b1cf58bd6837475
Yini <yinnski@gmail.com>
2014-07-25 02:54:24 +0200

    added action filter attribute for token validation

---
+++ security/TokenValidationAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using vDieu.Common.Utils;
+ 
+namespace vDieu.Web.Filters
+{
+    public class TokenValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            string token;
+
+            try
+            {
+                token = actionContext.Request.Headers.GetValues("Authorization-Token").First();
+            }
+            catch (Exception)
+            {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Missing Authorization-Token")
+                };
+                return;
+            }
+
+            try
+            {
+				// TODO: here we use simple membership provider, 
+				// replace with your own authentication. 
+                if (!WebSecurity.UserExists(RsaCrypto.Decrypt(token)))
+                {
+                    throw new Exception() ;
+                }
+                base.OnActionExecuting(actionContext);
+            }
+            catch (Exception)
+            {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
+                {
+                    Content = new StringContent("Unauthorized User")
+                };
+            }
+        }
+    }
+}
