cd25a49d46077fc89b706d192fb22d5beda5bed4
Yini <yinnski@gmail.com>
2014-07-25 02:26:57 +0200

    added a console program to generate new rsa keys

---
+++ security/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace RsaKeyGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            var privateParameters = rsa.ExportParameters(true);
+            var publicParameters = rsa.ExportParameters(false);
+   
+            //Export private parameter XML representation of privateParameters
+            //object created above
+            Debug.WriteLine(rsa.ToXmlString(true));
+ 
+            //Export private parameter XML representation of publicParameters
+            //object created above
+            Debug.WriteLine(rsa.ToXmlString(false));
+        }
+    }
+}
