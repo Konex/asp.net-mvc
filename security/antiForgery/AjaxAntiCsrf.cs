bef241c4ae0fc458ea802d22cc68ee727b05c2dc
Yini <yinnski@gmail.com>
2014-07-25 03:02:43 +0200

    added ajax anti forgery class

---
+++ security/antiForgery/AjaxAntiCsrf.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Helpers;
+
+namespace vDieu.Web.Utils.Security
+{
+    // Ajax wire-up on the view:
+    /*<script>
+    @functions{
+        public string TokenHeaderValue()
+        {
+            string cookieToken, formToken;
+            AntiForgery.GetTokens(null, out cookieToken, out formToken);
+            return cookieToken + ":" + formToken;                
+        }
+    }
+
+    $.ajax("api/values", {
+        type: "post",
+        contentType: "application/json",
+        data: {  }, // JSON data goes here
+        dataType: "json",
+        headers: {
+            'RequestVerificationToken': '@TokenHeaderValue()'
+        }
+    });
+    </script>*/
+    public class AjaxAntiCsrf
+    {
+        public static void ValidateRequestHeader(HttpRequestMessage request)
+        {
+            string cookieToken = "";
+            string formToken = "";
+
+            IEnumerable<string> tokenHeaders;
+            if (request.Headers.TryGetValues("RequestVerificationToken", out tokenHeaders))
+            {
+                string[] tokens = tokenHeaders.First().Split(':');
+                if (tokens.Length == 2)
+                {
+                    cookieToken = tokens[0].Trim();
+                    formToken = tokens[1].Trim();
+                }
+            }
+            AntiForgery.Validate(cookieToken, formToken);
+        }
+    }
+}
