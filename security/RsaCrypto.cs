2fd3f71b0b1b5522a4b915accf6dd23963eb051f
Yini <yinnski@gmail.com>
2014-07-25 02:22:50 +0200

    Added an RSA encryption class

---
+++ security/RsaCrypto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace vDieu.Common.Utils
+{
+    public class RsaCrypto
+    {
+        //TODO: replace the public and private keys for production
+        private const string PrivateKey = "<RSAKeyValue><Modulus>s6lpjspk+3o2GOK5TM7JySARhhxE5gB96e9XLSSRuWY2W9F951MfistKRzVtg0cjJTdSk5mnWAVHLfKOEqp8PszpJx9z4IaRCwQ937KJmn2/2VyjcUsCsor+fdbIHOiJpaxBlsuI9N++4MgF/jb0tOVudiUutDqqDut7rhrB/oc=</Modulus><Exponent>AQAB</Exponent><P>3J2+VWMVWcuLjjnLULe5TmSN7ts0n/TPJqe+bg9avuewu1rDsz+OBfP66/+rpYMs5+JolDceZSiOT+ACW2Neuw==</P><Q>0HogL5BnWjj9BlfpILQt8ajJnBHYrCiPaJ4npghdD5n/JYV8BNOiOP1T7u1xmvtr2U4mMObE17rZjNOTa1rQpQ==</Q><DP>jbXh2dVQlKJznUMwf0PUiy96IDC8R/cnzQu4/ddtEe2fj2lJBe3QG7DRwCA1sJZnFPhQ9svFAXOgnlwlB3D4Gw==</DP><DQ>evrP6b8BeNONTySkvUoMoDW1WH+elVAH6OsC8IqWexGY1YV8t0wwsfWegZ9IGOifojzbgpVfIPN0SgK1P+r+kQ==</DQ><InverseQ>LeEoFGI+IOY/J+9SjCPKAKduP280epOTeSKxs115gW1b9CP4glavkUcfQTzkTPe2t21kl1OrnvXEe5Wrzkk8rA==</InverseQ><D>HD0rn0sGtlROPnkcgQsbwmYs+vRki/ZV1DhPboQJ96cuMh5qeLqjAZDUev7V2MWMq6PXceW73OTvfDRcymhLoNvobE4Ekiwc87+TwzS3811mOmt5DJya9SliqU/ro+iEicjO4v3nC+HujdpDh9CVXfUAWebKnd7Vo5p6LwC9nIk=</D></RSAKeyValue>";
+        private const string PublicKey = "<RSAKeyValue><Modulus>s6lpjspk+3o2GOK5TM7JySARhhxE5gB96e9XLSSRuWY2W9F951MfistKRzVtg0cjJTdSk5mnWAVHLfKOEqp8PszpJx9z4IaRCwQ937KJmn2/2VyjcUsCsor+fdbIHOiJpaxBlsuI9N++4MgF/jb0tOVudiUutDqqDut7rhrB/oc=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+        private static readonly UnicodeEncoding Encoder = new UnicodeEncoding();
+ 
+        public static string Decrypt(string data)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(PrivateKey);
+            var decryptedBytes = rsa.Decrypt(Convert.FromBase64String(data), false);
+            return Encoder.GetString(decryptedBytes);
+        }
+
+        public static string Encrypt(string data)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(PublicKey);
+            var dataToEncrypt = Encoder.GetBytes(data);
+            var encryptedBytes = rsa.Encrypt(dataToEncrypt, false);
+            return Convert.ToBase64String(encryptedBytes);
+        }
+    }
+}
+
