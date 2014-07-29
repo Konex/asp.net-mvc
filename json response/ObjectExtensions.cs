95338ab733f028e26c24a4c8a815798561c4f025
YiniYin <yiniski@gmail.com>
2014-07-30 00:17:32 +0200

    Update ObjectExtensions.cs

--- json response/ObjectExtensions.cs
+++ json response/ObjectExtensions.cs
@@ -13,24 +13,24 @@ namespace vDieu
         {
             var settings = new JsonSerializerSettings()
             {
-			    // Json.NET will ignore objects in reference loops and not serialize them. 
-				// The first time an object is encountered it will be serialized as usual 
-				// but if the object is encountered as a child object of itself the serializer 
-				// will skip serializing it.
+		// Json.NET will ignore objects in reference loops and not serialize them. 
+		// The first time an object is encountered it will be serialized as usual 
+		// but if the object is encountered as a child object of itself the serializer 
+		// will skip serializing it.
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-				// maximum depth allowed when reading JSON.
+		// maximum depth allowed when reading JSON.
                 MaxDepth = 1,
                 PreserveReferencesHandling = PreserveReferencesHandling.None
             };
 			
-			// TODO: set the date format in your culture. Here is set for Australia date.
+	    // TODO: set the date format in your culture. Here is set for Australia date.
             var timeConverter = new IsoDateTimeConverter {DateTimeFormat = "dd-MM-yyyy HH:mm:ss"};
             settings.Converters.Add(timeConverter);
 
             return = JsonConvert.SerializeObject(obj, Formatting.Indented, settings);
         }
 		
-		 public static string ToJsonNet(this object obj, JsonSerializerSettings settings, 
+	public static string ToJsonNet(this object obj, JsonSerializerSettings settings, 
             Formatting formatting, IsoDateTimeConverter dateTimeConverter)
         {
             settings.Converters.Add(dateTimeConverter);
