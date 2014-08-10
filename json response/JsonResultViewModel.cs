8ffb13312f08303745e9186e0c6cbeddf8ca3745
Yini <yinnski@gmail.com>
2014-08-11 00:01:03 +0200

    added JsonResultViewModel class

---
+++ json response/JsonResultViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace vDieu.Web.Models
+{
+    public class JsonResultViewModel<T>
+    {
+        public JsonResultViewModel()
+        {
+        }
+
+        public JsonResultViewModel(bool isSuccessful, T data)
+        {
+            Data = data;
+        }
+        public JsonResultViewModel(bool isSuccessful, string msg = "")
+        {
+            Success = isSuccessful;
+            Message = msg;
+        }
+
+        [JsonProperty("isSuccessful")]
+        public bool IsSuccessful { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("data")]
+        public T Data { get; set; }
+
+        [JsonProperty("modelState")]
+        public Dictionary<string, string> ModelState { get; set; }
+    }
+
+}
